ce709f75b0a16d237c92232ac772756b83753e5c
Andrew <andrewmll94@gmail.com>
2022-11-04 09:14:59 -0300

    Conclusão do requisito 5 :rocket:

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,51 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    userName: '',
+    isButtonDisable: true,
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      userName: target.value,
+    });
+  };
+
+  // handleButton = async () => {
+  //   const { userName } = this.state;
+  //   this.setState({
+  //     loading: true,
+  //   });
+  //   await createUser({ name: userName });
+  //   this.setState({
+  //     loading: false,
+  //     load: true,
+  //   });
+  // };
+
   render() {
+    const { userName, isButtonDisable } = this.state;
+    const MIN_CHARACTERS = 2;
     return (
-      <div data-testid="page-search">
+      <div data-testid="page-login">
         <Header />
-        <h2>Search</h2>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            placeholder="Nome"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ userName.length >= MIN_CHARACTERS ? false : isButtonDisable }
+            onClick={ this.handleButton }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
