3c4242b6fc1f251c963a3a0a0789386cd0af66b8
Andrew <andrewmll94@gmail.com>
2022-11-03 23:19:25 -0300

    Conclusão do requisito 3 :rocket:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends Component {
+  state = {
+    userName: '',
+    loading: true,
+  };
+
+  getName = async () => {
+    const name = await getUser();
+    this.setState({
+      userName: name.name,
+      loading: false,
+    });
+  };
+
   render() {
+    this.getName();
+    const { userName, loading } = this.state;
     return (
       <header data-testid="header-component">
+        <span data-testid="header-user-name">{ loading ? <Loading /> : userName }</span>
         <h1>TrybeTunes</h1>
         <Link to="/search">Search</Link>
         <Link to="/album/:id">Album</Link>
